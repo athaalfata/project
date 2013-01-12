9b014618c6a1aae6765a62969fd8cf4303864139
athaalfata <anto24_91@yahoo.co.id>
2013-01-12 09:33:45 +0700

    Revert "updasupdoad"
    This reverts commit b846aa7a9194501e3130e79509d32a6cd95e035d.

--- views/pilihTypeProject.php
+++
@@ -1,8 +0,0 @@
-<div align="center">
-<h3>Pilih Type Project</h3>
-<?php echo form_open('project/project/cekSubmit'); ?>
-<input class="btn btn-large" type="submit" value="Regular" name="regular">  
-<input class="btn btn-large" type="submit" value="Trial" name="trial">   
-<input class="btn btn-large" type="submit" value="Kontes" name="kontes">
-<?php echo form_close(); ?>
-</div>
