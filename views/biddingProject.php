4d458514383c2245441636519afd051b377aa29b
athaalfata <anto24_91@yahoo.co.id>
2013-01-12 09:35:57 +0700

    updated project
    12-01-13

---
+++ views/biddingProject.php
@@ -0,0 +1,32 @@
+<h3>Bidding Project</h3>
+<form>
+<table class="table">
+	<tr>
+		<td>Nama Project</td>
+		<td>:</td>
+		<td>Aplikasi Mobile</td>
+	</tr>
+	<tr>
+		<td>Budget</td>
+		<td>:</td>
+		<td>< Rp.10.000.000,-</td>
+	</tr>
+	<tr>
+		<td>Tawaran Anda</td>
+		<td>:</td>
+		<td>Rp. <input type="text" placeholder="Tawaran Anda"></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td>
+			<div class="control-group">
+			    <div class="controls">
+			      <button type="submit" class="btn btn-info">Bid !</button>
+			      <button type="submit" class="btn">Back</button>
+			    </div>
+			</div>
+		</td>	
+	</tr>
+</table>
+</form>
---
+++ views/tambahProjectTrial1.php
@@ -0,0 +1,58 @@
+<h3>Tambah Project Trial</h3>
+<form class="form-horizontal">
+  <div class="control-group">
+    <label class="control-label" for="inputEmail">Nama Project</label>
+    <div class="controls">
+      <input type="text" id="inputEmail" placeholder="Masukkan nama Project">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputEmail">Kategori</label>
+    <div class="controls">
+      <select multiple="multiple">
+        <option>Websites, IT & Software</option>
+        <option>Mobile Phones & Computing</option>
+        <option>Writing & Content</option>
+        <option>Design, Media & Architecture</option>
+        <option>Data Entry & Admin</option>
+        <option>Engineering & Science</option>
+        <option>Product Sourcing & Manufacturing</option>
+        <option>Sales & Marketing</option>
+        <option>Business, Accounting, Human Resources & Legal</option>
+        <option>Translation & Languages</option>
+        <option>Other</option>
+      </select>
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputDeskripsi">Deskripsi</label>
+    <div class="controls">
+      <textarea rows="3"></textarea>
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputSkill">Skill yang dibutuhkan</label>
+    <div class="controls">
+      <input type="text" id="inputSkill" placeholder="">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputEmail">Budget yang tersedia</label>
+    <div class="controls">
+      <select multiple="multiple">
+        <option name="1">Kurang dari Rp. 5.000.000,00</option>
+        <option name="2">Rp. 5.000.000,00 - Rp. 10.000.000,00</option>
+        <option name="3">Rp. 10.000.000,00 - Rp. 50.000.000,00</option>
+        <option name="4">Rp. 50.000.000,00 - Rp. 100.000.000,00</option>
+        <option name="5">Lebih dari Rp. 100.000.000,00</option>
+        <option name="6">Tidak Yakin</option>
+      </select>
+    </div>
+  </div>
+  <div class="control-group">
+    <div class="controls">
+      <button type="submit" class="btn btn-info">Next</button>
+      <button type="submit" class="btn btn-danger">Cancel</button>
+    </div>
+  </div>
+</form>
